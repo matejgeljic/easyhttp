398684f109c492e7ecd44e3d64f8f98883d6bd15
matejgeljic <32310104+matejgeljic@users.noreply.github.com>
2018-09-03 23:31:51 +0200

    Add files via upload

---
+++ fetch with promises/app2.js
@@ -0,0 +1,28 @@
+const http = new EasyHTTP;
+
+//Get Users
+http.get('https://jsonplaceholder.typicode.com/users')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+//User Data
+const data = {
+    name: 'Matej Geljic',
+    username: 'matejgljc',
+    email: 'mgeljic@gmail.com'
+}
+
+//Create User
+http.post('https://jsonplaceholder.typicode.com/users', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+//Update User
+http.put('https://jsonplaceholder.typicode.com/users/7', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+//Delete User
+http.delete('https://jsonplaceholder.typicode.com/users/3')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
